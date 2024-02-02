0143385961c0bf4b9c16724c52ca8ba1fbdc3ff3
kirankumarhexafoldtech <kirankumar.challagiri@hexafoldtech.com>
2024-02-02 17:22:48 +0530

    change in home section

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default defineComponent({
     render () {
       return <div>
 
-        <h1>Welcome to ramcash home.</h1>
+        <h1>Welcome to remcash home.</h1>
         <NuxtLink to={"/login"}>Login</NuxtLink>
       </div>
     }
