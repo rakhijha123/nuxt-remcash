4cd4f2e9f846e3d0f39d6c9fb65c98feb9782bd4
kirankumarhexafoldtech <kirankumar.challagiri@hexafoldtech.com>
2024-02-05 19:20:14 +0530

    animation not added

--- app.vue
+++ app.vue
@@ -2,3 +2,4 @@
     <NuxtLayout />
     <NuxtPage />
 </template>
+
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -3,32 +3,39 @@ import RamecashLogo from "@/assets/svg/logo01.svg";
 import Wallet from "@/assets/svg/wallet.svg";
 import PayBills from "@/assets/svg/pay_bills.svg";
 import MoneyTransfer from "@/assets/svg/instant_money_tranfer.svg";
+
 export default defineComponent({
   setup() {
-   const leftScrollContent = reactive([
+    const imageRef = ref<HTMLImageElement | null>(null);
+    const leftScrollContent = reactive([
       {
-         img:Wallet,
-         heading:"Secure digital wallet",
-         description:"Remcash provides a fully secure digital wallet to ease everyday transactions"
+        img: Wallet,
+        heading: "Secure digital wallet",
+        description:
+          "Remcash provides a fully secure digital wallet to ease everyday transactions",
       },
       {
-         img:PayBills,
-         heading:"Instant money transfer",
-         description:"Remcash provides fast, secure and instant money transfers across the globe"
+        img: PayBills,
+        heading: "Instant money transfer",
+        description:
+          "Remcash provides fast, secure and instant money transfers across the globe",
       },
       {
-         img:MoneyTransfer,
-         heading:"Pay the bills easily",
-         description:"With Remcash, paying bills is quick and easy, fully secure and convenient"
-      }
-   ]);
-   const leftScrollContentPosition = ref(0);
-   const leftScroll = () => {
-      leftScrollContentPosition.value = (leftScrollContentPosition.value + 1) % leftScrollContent.length;
+        img: MoneyTransfer,
+        heading: "Pay the bills easily",
+        description:
+          "With Remcash, paying bills is quick and easy, fully secure and convenient",
+      },
+    ]);
+    const leftScrollContentPosition = ref(0);
+    const leftScroll = () => {
+      // Add data attribute to trigger transition
+      leftScrollContentPosition.value =
+        (leftScrollContentPosition.value + 1) % leftScrollContent.length;
+      const img = document.querySelector(".leftImage");
     };
-   onMounted(() => {
+    onMounted(() => {
       const intervalId = setInterval(leftScroll, 5000); // Change image every 5000 milliseconds (5 seconds)
-
       // Clear the interval when the component is unmounted
       onBeforeUnmount(() => {
         clearInterval(intervalId);
@@ -38,16 +45,27 @@ export default defineComponent({
   },
   render() {
     return (
-       <div class="main_login">
+      <div class="main_login">
+       
         <div class="main_login_left">
           <div class="main_login_left-top">
-            <img src={this.leftScrollContent[this.leftScrollContentPosition].img} alt="" />
+            <img
+              src={this.leftScrollContent[this.leftScrollContentPosition].img}
+              alt=""
+              ref="imageRef"
+              class="leftImage"
+            />
           </div>
-          
+
           <div class="main_login_left-bottom">
-            <h2>{this.leftScrollContent[this.leftScrollContentPosition].heading}</h2>
+            <h2>
+              {this.leftScrollContent[this.leftScrollContentPosition].heading}
+            </h2>
             <p>
-            {this.leftScrollContent[this.leftScrollContentPosition].description}
+              {
+                this.leftScrollContent[this.leftScrollContentPosition]
+                  .description
+              }
             </p>
           </div>
         </div>
