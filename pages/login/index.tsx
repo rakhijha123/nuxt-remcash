82daff4a632c2d5289ac52a4bc5efaa3df7814f6
kirankumarhexafoldtech <kirankumar.challagiri@hexafoldtech.com>
2024-02-05 16:24:22 +0530

    completed with design next step animation

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -4,40 +4,80 @@ import Wallet from "@/assets/svg/wallet.svg";
 import PayBills from "@/assets/svg/pay_bills.svg";
 import MoneyTransfer from "@/assets/svg/instant_money_tranfer.svg";
 export default defineComponent({
-    render() {
-        return <div class="main_login">
-           <div class="main_login_left">
-            <div class="main_login_left-top">
-               <img src={Wallet} alt="" />
-            </div>
-            <div class="main_login_left-bottom">
-               <h2>Pay the bills easily</h2>
-               <p>Remcash provides a fully secure digital wallet to ease everyday transaction</p>
-            </div>
-           </div>
-           <div class="main_login_right">
-             <div class="main_login_form">
-                <div class="main_login_logo"><img src={RamecashLogo} alt="" />
-                <p>Remcash</p>
-                </div>
-                <div class="main_login_logintext">
-                  <h2>Log in</h2>
-                  <p>(Please fill the information below)</p>
-                </div>
-                  <form>
-                     <label>Phone number</label>
-                     <input type="text" class="main_login_input"/>
-                     <label>Password</label>
-                     <input type="password" class="main_login_input"/>
-                     <button>Log into your account</button>
-                  </form>
-                <p class="main_login_forgotpassword">Forgot password ?</p>
-                <p class="main_login_reg-login">Don't have an account? <span>Rigister here</span></p>
-             </div>
-             <div class="main_login_background">
-                <img src={BackgroundImage} alt="" />
-             </div>
-           </div>
+  setup() {
+   const leftScrollContent = reactive([
+      {
+         img:Wallet,
+         heading:"Secure digital wallet",
+         description:"Remcash provides a fully secure digital wallet to ease everyday transactions"
+      },
+      {
+         img:PayBills,
+         heading:"Instant money transfer",
+         description:"Remcash provides fast, secure and instant money transfers across the globe"
+      },
+      {
+         img:MoneyTransfer,
+         heading:"Pay the bills easily",
+         description:"With Remcash, paying bills is quick and easy, fully secure and convenient"
+      }
+   ]);
+   const leftScrollContentPosition = ref(0);
+   const leftScroll = () => {
+      leftScrollContentPosition.value = (leftScrollContentPosition.value + 1) % leftScrollContent.length;
+    };
+   onMounted(() => {
+      const intervalId = setInterval(leftScroll, 5000); // Change image every 5000 milliseconds (5 seconds)
+
+      // Clear the interval when the component is unmounted
+      onBeforeUnmount(() => {
+        clearInterval(intervalId);
+      });
+    });
+    return { leftScrollContent, leftScrollContentPosition };
+  },
+  render() {
+    return (
+       <div class="main_login">
+        <div class="main_login_left">
+          <div class="main_login_left-top">
+            <img src={this.leftScrollContent[this.leftScrollContentPosition].img} alt="" />
+          </div>
+          
+          <div class="main_login_left-bottom">
+            <h2>{this.leftScrollContent[this.leftScrollContentPosition].heading}</h2>
+            <p>
+            {this.leftScrollContent[this.leftScrollContentPosition].description}
+            </p>
+          </div>
         </div>
-    }
-})
+        <div class="main_login_right">
+          <div class="main_login_form">
+            <div class="main_login_logo">
+              <img src={RamecashLogo} alt="" />
+              <p>Remcash</p>
+            </div>
+            <div class="main_login_logintext">
+              <h2>Log in</h2>
+              <p>(Please fill the information below)</p>
+            </div>
+            <form>
+              <label>Phone number</label>
+              <input type="text" class="main_login_input" />
+              <label>Password</label>
+              <input type="password" class="main_login_input" />
+              <button>Log into your account</button>
+            </form>
+            <p class="main_login_forgotpassword">Forgot password ?</p>
+            <p class="main_login_reg-login">
+              Don't have an account? <span>Rigister here</span>
+            </p>
+          </div>
+          <div class="main_login_background">
+            <img src={BackgroundImage} alt="" />
+          </div>
+        </div>
+      </div>
+    );
+  },
+});
