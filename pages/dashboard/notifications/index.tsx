35311ccaf4a5ade1725cd658011638558d8d6c6c
kirankumarhexafoldtech <ch.kirankumar311@gmail.com>
2024-02-19 11:35:25 +0530

    notifications folder added

---
+++ pages/dashboard/notifications/index.tsx
@@ -0,0 +1,34 @@
+import Navbar from "@/layouts/navbar";
+import Sidebar from "@/layouts/dashboard/sidebar";
+import { useSidebar } from "@/composables/states";
+
+export default defineComponent({
+    components:{
+
+    },
+    setup(props, ctx) {
+        const sidebar = useSidebar();
+
+        return {
+            sidebar
+        }
+    },
+    render(){
+        return (
+        <div class="dashboard">
+            <Sidebar/>
+            <Navbar class="dashboard-navbar"/>
+            <div
+          class={
+            this.sidebar
+              ? "dashboard-main"
+              : "dashboard-main dashboard-main-inactive"
+          }
+        >
+          {/* Todo here */}
+
+        </div>
+        </div>
+        )
+    }
+})
