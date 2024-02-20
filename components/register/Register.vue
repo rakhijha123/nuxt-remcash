a3595fe7f0b520322f360d525e59de051ec454f0
kirankumarhexafoldtech <ch.kirankumar311@gmail.com>
2024-02-20 12:49:00 +0530

    registration added

---
+++ components/register/Register.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="register">
+        <div class="main_auth_logintext">
+     <h2>Register</h2>
+     <p>(Please fill the information below)</p>
+   </div>
+   <form @submit.prevent="">
+     <label>Last name</label>
+     <input
+       type="text"
+       class="main_auth_input"
+       v-model="userCred.last_name"
+       placeholder="Your last name..."
+     />
+     <label>First name</label>
+     <input
+       type="text"
+       class="main_auth_input"
+       v-model="userCred.first_name"
+       placeholder="Your first name..."
+     />
+        <label>Phone number</label>
+     <input
+       type="text"
+       class="main_auth_input"
+       v-model="userCred.phone"
+       placeholder="For example: 495754.."
+     />
+       <label>Email address (optional)</label>
+     <input
+       type="email"
+       class="main_auth_input"
+       v-model="userCred.email"
+       placeholder="Your email address..."
+     />
+     <div class="register_checkbox"> 
+      <div class="register_checkbox_input" @click="terms.checked = !terms.checked">
+        <i class="ri-checkbox-fill" v-if="terms.checked"></i>
+      </div>
+       <p>By checking the box, I agree to the {{" "}}
+         <span>
+          Terms & Conditions
+         </span>
+         {{" "}} of Remcash.</p>
+     </div>
+     <button
+       @click=""
+     >
+       Confirm and proceed
+     </button>
+   </form>
+   <p class="main_auth_reg-login">
+     Already user?{{" "}}
+     <span>
+       <NuxtLink to="/auth/access">
+       Login here
+       </NuxtLink>
+     </span>
+   </p>
+ </div>
+</template>
+
+<script setup lang="ts">
+ const router = useRouter();
+    const terms = ref({
+      checked:false
+    })
+    // login payload parameters
+    const userCred = ref({
+      first_name: "",
+      last_name: "",
+      phone:"",
+      email:"",
+    });
+    // Login form
+    const registerForm = (e: any) => {
+      e.preventDefault();
+      console.log(userCred.value);
+    };
+</script>
---
+++ pages/auth/register/index.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="main_auth">
+        <div class="main_auth_left">
+          <div class="main_auth_left-top">
+            <img
+              :src="leftScrollContent[leftScrollContentPosition].img"
+              alt=""
+              ref="imageRef"
+              class="leftImage"
+            />
+          </div>
+          <div class="main_auth_left-bottom">
+            <h2>
+              {{leftScrollContent[leftScrollContentPosition].heading}}
+            </h2>
+            <p>
+              {{
+                leftScrollContent[leftScrollContentPosition]
+                  .description
+              }}
+            </p>
+          </div>
+        </div>
+        <div class="main_auth_right">
+          <div class="main_auth_form" >
+            <div class="register-bar">
+              <div class="register-bar-point register-bar-point-blink"></div>
+              <div class="register-bar-point"></div>
+              <div class="register-bar-point"></div>
+              <div class="register-bar-point"></div>
+            </div>
+          <!-- <div style={{margin:"30px auto 0 auto",width:"350px",height:'10px',backgroundColor:"#E4EFFA",display:"flex",flexDirection:"row",justifyContent:"space-between",marginBottom:"50px"}}>
+            <div style={{width:"25px",height:"25px",borderRadius:"50%",position:"relative",bottom:"8px"}} class="colors"></div>
+            <div style={{width:"25px",height:"25px",backgroundColor:"#E4EFFA",borderRadius:"50%",position:"relative",bottom:"8px"}}></div>
+            <div style={{width:"25px",height:"25px",backgroundColor:"#E4EFFA",borderRadius:"50%",position:"relative",bottom:"8px"}}></div>
+            <div style={{width:"25px",height:"25px",backgroundColor:"#E4EFFA",borderRadius:"50%",position:"relative",bottom:"8px"}}></div>
+          </div> -->
+            <div class="main_auth_logo">
+              <img :src="RamecashLogo" alt="" />
+              <p>Remcash</p>
+            </div>
+               <Register/>
+          </div>
+          <div class="main_auth_background">
+            <img :src="BackgroundImage" alt="" />
+          </div>
+        </div>
+      </div>
+</template>
+
+<script setup lang="ts">
+import Wallet from "@/assets/svg/wallet.svg";
+import PayBills from "@/assets/svg/pay_bills.svg";
+import MoneyTransfer from "@/assets/svg/instant_money_tranfer.svg";
+import BackgroundImage from "@/assets/svg/bg_glob.svg";
+import RamecashLogo from "@/assets/svg/logo01.svg";
+
+const leftScrollContent = reactive([
+      {
+        img: Wallet,
+        heading: "Secure digital wallet",
+        description:
+          "Remcash provides a fully secure digital wallet to ease everyday transactions",
+      },
+      {
+        img: PayBills,
+        heading: "Instant money transfer",
+        description:
+          "Remcash provides fast, secure and instant money transfers across the globe",
+      },
+      {
+        img: MoneyTransfer,
+        heading: "Pay the bills easily",
+        description:
+          "With Remcash, paying bills is quick and easy, fully secure and convenient",
+      },
+    ]);
+    // carosel conttent reference variable
+    const leftScrollContentPosition = ref(0);
+    // carousel content method
+    const leftScroll = () => {
+      // Add data attribute to trigger transition
+      leftScrollContentPosition.value =
+        (leftScrollContentPosition.value + 1) % leftScrollContent.length;
+    };
+    // functions
+    const handleRegisterView = (data:boolean) =>{
+    //    showForm.value = "register"
+    } 
+    // life cycle hooks
+    onMounted(() => {
+      const intervalId = setInterval(leftScroll, 5000); // Change image every 5000 milliseconds (5 seconds)
+      // Clear the interval when the component is unmounted
+      onBeforeUnmount(() => {
+        clearInterval(intervalId);
+      });
+    });
+
+
+definePageMeta({
+  layout: false
+})
+</script>   
