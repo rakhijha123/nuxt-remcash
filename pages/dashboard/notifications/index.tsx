ad3db69215fdc3bf8dd1cf5a5f97f5611c251d7a
rakhijha123 <rakhi.jha@hexafoldtech.com>
2024-02-20 13:17:49 +0530

    notification-page

--- pages/dashboard/notifications/index.tsx
+++ pages/dashboard/notifications/index.tsx
@@ -25,7 +25,98 @@ export default defineComponent({
           }
         >
           {/* Todo here */}
+        <div class="notification">
+          <div class="notification-content">
+            <div class="notification-header-container">
+              <div class="notification-header-text">
+                <h1>Notifications</h1>
+                <p>Please provide the information below</p>
+              </div>
+              <div class="notification-header-serch-input">
+              <i class="ri-search-line"></i>
+              <input placeholder="Search for contact..." />
+              </div>
 
+              
+              </div>
+              <div class="notification-table">
+              <table>
+         
+        <tr>
+           
+            <td class="td-info"><span>icon</span> Transfered money to Adolphus Laryea</td>
+            <td class="td-date-time">26/08/2021 10:43Am <span>icon</span></td>
+          
+          
+            
+        </tr>
+      
+        <tr>
+    
+            <td class="td-info"><span>icon</span> Amal credited your Ecobank Account</td>
+            <td class="td-date-time">26/08/2021 10:43Am <span>icon</span></td>
+            
+            
+        </tr>
+   
+        <tr>
+        
+            <td class="td-info"><span>icon</span> Uploaded money on account</td>
+            <td class="td-date-time">26/08/2021 10:43Am <span>icon</span></td>
+
+                   </tr>
+       
+        <tr>
+            <td class="td-info"><span>icon</span> Withdrawed money from account</td>
+            <td class="td-date-time">26/08/2021 10:43Am <span>icon</span></td>
+          
+        </tr>
+       
+        <tr>
+            <td class="td-info"><span>icon</span> Withdrawed money from account</td>
+            <td class="td-date-time">26/08/2021 10:43Am <span>icon</span></td>
+         
+            
+        </tr>
+        <tr>
+            <td class="td-info"><span>icon</span> Withdrawed money from account</td>
+            <td class="td-date-time">26/08/2021 10:43Am <span>icon</span></td>
+          
+            
+        </tr>
+      
+        <tr>
+            <td class="td-info"><span>icon</span> Transfered money to Debbie Lewis</td>
+            <td class="td-date-time">26/08/2021 10:43Am <span>icon</span></td>
+            
+        </tr>
+   
+        <tr>
+            <td class="td-info"><span>icon</span> Withdrawed money from account</td>
+            <td class="td-date-time">26/08/2021 10:43Am <span>icon</span></td>
+          
+           
+            
+        </tr>
+       
+        <tr>
+            <td class="td-info"><span>icon</span> Uploaded money on account</td>
+            <td class="td-date-time">26/08/2021 10:43Am <span>icon</span></td>
+            
+          
+        </tr>
+       
+        <tr>
+          <td class="td-info"><span>icon</span> Withdrawed money from account</td>
+            <td class="td-date-time">26/08/2021 10:43Am <span>icon</span></td>
+          
+         
+            
+        </tr>
+    </table>  
+            </div>
+          </div>
+        </div>
         </div>
         </div>
         )
